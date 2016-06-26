179442b4f9114407d8b714c38e5bde82c1f8566b
Martin Wiegand <dm@freibrief.net>
2016-06-26 14:55:31 +0200

    random commit

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -4,32 +4,48 @@ class ACL # Access Controll List
     @acl
   end
 
-  def allows?(action, category)
-    @acl[:categories][category.id] and @acl[:categories][category.id].include?(action.to_sym)
+  def allows?(action, scopable)
+    scopable_type = scopable.model_name.route_key.to_sym
+    @acl[scopable_type][scopable.id] and @acl[scopable_type][scopable.id].include?(action.to_sym)
   end
   
   def visible_categories
     @acl[:visible]
   end
 
-private
+  private
 
+  def all_memberships user
+    Recursion.collect_all(user.groups, :groups)
+  end
+  
   def build_for user
     @acl = {
       categories: {},
+      groups: {},
       visible: []
     }
     
+    scopable_ids = []
+    
+    # collect groups
     Recursion.collect_all(user.groups, :groups).each do |group|
       group.role_scopes.each do |role_scope|
-        c_ids = [role_scope.scopable.id]
-        c_ids << Recursion.collect(role_scope.scopable, :categories).pluck(:id) if role_scope.recursive
+        # category- or group-permission?
+        scopable_type = role_scope.scopable.model_name.route_key.to_sym
         
-        c_ids.flatten.each do |c_id|
-          @acl[:categories][c_id] = [] unless @acl[:categories][c_id]
+        # collect affected scopables
+        scopable_ids << [role_scope.scopable.id]
+        scopable_ids << Recursion.collect(role_scope.scopable, scopable_type).pluck(:id) if role_scope.recursive
+        
+        # insert permissions into acl
+        scopable_ids.flatten.uniq.each do |scopable_id|
+          # scopable-key exists?
+          @acl[scopable_type][scopable_id] = [] unless @acl[scopable_type][scopable_id]
+          # insert permissions
           role_scope.role.permissions.each do |permission|
-            @acl[:categories][c_id] << permission.action.to_sym
-            @acl[:visible] << c_id if permission.action.to_sym == :read_category
+            @acl[scopable_type][scopable_id] << permission.action.to_sym
+            @acl[:visible] << scopable_id if permission.action.to_sym == :read_category
           end
         end
       end
